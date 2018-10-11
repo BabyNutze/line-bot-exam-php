74a1521bb9e87dfcbcef35ea5d6215699207c8a0
BabyNutze <babysparker@live.com>
2018-10-11 15:37:07 +0700

    Update testbot.php

--- testbot.php
+++ testbot.php
@@ -66,6 +66,7 @@ function replyMsg($arrayHeader,$arrayPostData){
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $result = curl_exec($ch);
         curl_close ($ch);
+        print $result;
     }
    exit;
 ?>
