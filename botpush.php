fc690b2f3aa046647c1e8b2fb3c1b876e732930e
BabyNutze <babysparker@live.com>
2018-10-11 14:49:59 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -8,7 +8,7 @@ $access_token = 'xfdtD+noG4mK+lCu8TZMzSN2HS785dNvsejSBYNcPDJAJroO1CthWkWipqkuJ4a
 
 $channelSecret = 'c5a345849ccfdef0cc027ee11252aa7c';
 
-$pushID = 'U102d2918d61a46fb9e5b0ce72efd3c5d';
+$pushID = array('U102d2918d61a46fb9e5b0ce72efd3c5d','Uea0b6f3a1111d357fc0e7b2fb013db22');
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
