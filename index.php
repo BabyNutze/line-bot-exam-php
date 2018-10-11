ae0093f9b7ca9e91898b1623ae46111e090c5501
BabyNutze <babysparker@live.com>
2018-10-11 16:12:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
-print_r($text);
+print $text;
 print $result . "\r\n";
 }
 }
