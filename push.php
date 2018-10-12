9a0c12e06549ee3ee9d848de891707fb561083cd
BabyNutze <babysparker@live.com>
2018-10-12 10:38:52 +0700

    Update push.php

--- push.php
+++ push.php
@@ -1,12 +1,12 @@
 <?php
 
-$strAccessToken = "ACCESS_TOKEN";
+$access_token = 'xfdtD+noG4mK+lCu8TZMzSN2HS785dNvsejSBYNcPDJAJroO1CthWkWipqkuJ4aKQI8i4iwubJQF/EcoIcjEYfFc64KgAr4iswIr14Ijyg6olIVvP21VVEExKDY9fCqstZL+3Sd9BBTaWiBZcg6SJgdB04t89/1O/w1cDnyilFU';
 
-$strUrl = "https://api.line.me/v2/bot/message/push";
+$url = "https://api.line.me/v2/bot/message/push";
 
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+$arrHeader[] = "Authorization: Bearer {$access_token}";
 
 $arrPostData = array();
 $arrPostData['to'] = "U102d2918d61a46fb9e5b0ce72efd3c5d";
@@ -15,7 +15,7 @@ $arrPostData['messages'][0]['text'] = "Bienvenue";
 
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_URL,$url);
 curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
