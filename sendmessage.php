b4a9a6e8fdab38ba63fbd2ab6c82fb11d443ff27
BabyNutze <babysparker@live.com>
2018-10-12 11:39:04 +0700

    Update sendmessage.php

--- sendmessage.php
+++ sendmessage.php
@@ -15,25 +15,12 @@
                   <img src="https://image.ibb.co/kUagtU/rocket_contact.png" alt="rocket_contact"/>
               </div>
               <form method="post">
-                  <h3>Drop Us a Message</h3>
+                  <h3>Message</h3>
                  <div class="row">
                       <div class="col-md-6">
                           <div class="form-group">
-                              <input type="text" name="txtName" class="form-control" placeholder="Your Name *" value="" />
-                          </div>
-                          <div class="form-group">
-                              <input type="text" name="txtEmail" class="form-control" placeholder="Your Email *" value="" />
-                          </div>
-                          <div class="form-group">
-                              <input type="text" name="txtPhone" class="form-control" placeholder="Your Phone Number *" value="" />
-                          </div>
-                          <div class="form-group">
-                              <input type="submit" name="btnSubmit" class="btnContact" value="Send Message" />
-                          </div>
-                      </div>
-                      <div class="col-md-6">
-                          <div class="form-group">
-                              <textarea name="txtMsg" class="form-control" placeholder="Your Message *" style="width: 100%; height: 150px;"></textarea>
+                            <label for="textmsg">message:</label>
+                              <textarea name="txtMsg" class="form-control" ></textarea>
                           </div>
                       </div>
                   </div>
