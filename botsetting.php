4d2820f6a2fa8ed58a5b35be91ac082d6f311282
BabyNutze <babysparker@live.com>
2018-10-12 09:19:48 +0700

    Update botsetting.php

--- botsetting.php
+++
@@ -1,7 +0,0 @@
-<?php
-/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
-define('LINE_MESSAGE_CHANNEL_ID','1613422773');
-define('LINE_MESSAGE_CHANNEL_SECRET','c5a345849ccfdef0cc027ee11252aa7c
-');
-define('LINE_MESSAGE_ACCESS_TOKEN','xfdtD+noG4mK+lCu8TZMzSN2HS785dNvsejSBYNcPDJAJroO1CthWkWipqkuJ4aKQI8i4iwubJQF/EcoIcjEYfFc64KgAr4iswIr14Ijyg6olIVvP21VVEExKDY9fCqstZL+3Sd9BBTaWiBZcg6SJgdB04t89/1O/w1cDnyilFU');
-?>
