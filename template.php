2b9afcb15f2fb27aaf544cd81294351950cdbaa4
BabyNutze <babysparker@live.com>
2018-10-16 14:58:50 +0700

    Update template.php

--- template.php
+++ template.php
@@ -103,7 +103,7 @@ if(!is_null($events)){
                 $userMessage = strtolower($userMessage); // แปลงเป็นตัวเล็ก สำหรับทดสอบ
                 switch ($userMessage) {
                     case "t":
-                        $textReplyMessage = "Bot ตอบกลับคุณเป็นข้อความ";
+                        $textReplyMessage = "สวัสดีครับ";
                         $replyData = new TextMessageBuilder($textReplyMessage);
                         break;
                     case "i":
