e700d149c2550a9fdca687db370c6ada081d64be
BabyNutze <babysparker@live.com>
2018-10-16 14:32:33 +0700

    Update template.php

--- template.php
+++ template.php
@@ -120,8 +120,8 @@ if(!is_null($events)){
                         $audioUrl = "https://www.ninenik.com/line/S_6988827932080.wav";
                         $replyData = new AudioMessageBuilder($audioUrl,20000);
                         break;
-                    case "l":
-                        $placeName = "ที่ตั้งร้าน";
+                    case "l" :
+                        $placeName = "ตำแหน่งที่ตั้งสภาวิศวกร";
                         $placeAddress = "แขวง พลับพลา เขต วังทองหลาง กรุงเทพมหานคร ประเทศไทย";
                         $latitude = 13.76363767;
                         $longitude = 100.60656106;
