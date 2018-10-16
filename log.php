dba211b2cbb317165864593d0bd93ebaee3b7609
BabyNutze <babysparker@live.com>
2018-10-16 11:55:15 +0700

    Update log.php

--- log.php
+++ log.php
@@ -1,6 +1,6 @@
 <?php
 echo "hi";
-$myfile = fopen("me.txt", "w") or die("Unable to open file!");
+$myfile = fopen("user.txt", "w") or die("Unable to open file!");
 $txt = "John Doe\n";
 fwrite($myfile, $txt);
 $txt = "Jane Doe\n";
