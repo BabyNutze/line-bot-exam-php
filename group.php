6dac9b6367e4994b6560c9bb8929600e23b38a38
BabyNutze <babysparker@live.com>
2018-10-20 21:43:45 +0700

    Update group.php

--- group.php
+++ group.php
@@ -130,7 +130,7 @@ if(!is_null($events)){
                         $replyData = new VideoMessageBuilder($videoUrl,$picThumbnail);
                         break;
                     case "a":
-                        $audioUrl = "https://enigmatic-scrubland-34657.herokuapp.com/MarioInTrouble.wav";
+                        $audioUrl = "https://www.ninenik.com/line/S_6988827932080.wav";
                         $replyData = new AudioMessageBuilder($audioUrl,20000);
                         break;
                     case "ที่ตั้ง":
