eebe2c1daed54e149d382d0959a186ba93238b6e
BabyNutze <babysparker@live.com>
2018-10-20 20:43:11 +0700

    Update group.php

--- group.php
+++ group.php
@@ -111,9 +111,9 @@ if(!is_null($events)){
                         $replyData = new TextMessageBuilder($textReplyMessage);
                         break;
                     case "ผลบอล":
-                      $picFullSize = 'http://enigmatic-scrubland-34657.herokuapp.com/images/1360627.jpg';
-                      $picThumbnail = 'http://enigmatic-scrubland-34657.herokuapp.com/images/1360627_tn.jpg';
-                        $replyData = new ImageMessageBuilder($picFullSize,$picThumbnail);
+                       $picFullSize = 'http://enigmatic-scrubland-34657.herokuapp.com/images/1360627.jpg';
+                       $picThumbnail = 'http://enigmatic-scrubland-34657.herokuapp.com/images/1360627_tn.jpg';
+                       $replyData = new ImageMessageBuilder($picFullSize,$picThumbnail);
                         break;
                     case "v":
                         $picThumbnail = 'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/240';
