48ee7e36a10fbac72d32b5b62df57ae0fdd9ce01
BabyNutze <babysparker@live.com>
2018-10-22 14:28:40 +0700

    Update group.php

--- group.php
+++ group.php
@@ -133,14 +133,14 @@ if(!is_null($events)){
                         $audioUrl = "https://enigmatic-scrubland-34657.herokuapp.com/MarioInTrouble.wav";
                         $replyData = new AudioMessageBuilder($audioUrl,20000);
                         break;
-                    case "ที่ตั้ง" | | "อยู่ไหน":
+                    case ("ที่ตั้ง" | | "อยู่ไหน"):
                         $placeName = "ตำแหน่งตำแหน่งที่ตั้งสภาวิศวกร";
                         $placeAddress = "487/1 ซอยรามคำแหง 39 แขวงพลับพลา เขตวังทองหลาง กรุงเทพมหานคร ประเทศไทย";
                         $latitude = 13.780401863217657;
                         $longitude = 100.61141967773438;
                         $replyData = new LocationMessageBuilder($placeName, $placeAddress, $latitude ,$longitude);
                         break;
-                    case "m" || "mn":
+                    case ("m" || "mn"):
                         $textReplyMessage = "Bot ตอบกลับคุณเป็นข้อความ";
                         $textMessage = new TextMessageBuilder($textReplyMessage);
 
