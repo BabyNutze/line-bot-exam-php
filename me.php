9760022353712f1b82fde6ecada0529ef3b0b4a0
BabyNutze <babysparker@live.com>
2018-10-25 09:18:49 +0700

    Update me.php

--- me.php
+++ me.php
@@ -1,14 +1,14 @@
 <!doctype html>
-<?php  
-$mysqli = new mysqli("localhost", "me","1234","meeting");  
-/* check connection */ 
-if ($mysqli->connect_errno) {  
-    printf("Connect failed: %s\n", $mysqli->connect_error);  
-    exit();  
-}  
-if(!$mysqli->set_charset("utf8")) {  
-    printf("Error loading character set utf8: %s\n", $mysqli->error);  
-    exit();  
+<?php
+$mysqli = new mysqli("203.151.145.146", "me","","meeting");  
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+if(!$mysqli->set_charset("utf8")) {
+    printf("Error loading character set utf8: %s\n", $mysqli->error);
+    exit();
 }
 ?>
 <html lang="en">
@@ -27,16 +27,16 @@ if(!$mysqli->set_charset("utf8")) {
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
+
+
+
+
+
+
+
+
+
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
